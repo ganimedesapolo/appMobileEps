a154c02aa7f8db993827a628d8cd03790212a6f0
ganimedesapolo <diegoerleyduran@yahoo.com>
2018-08-24 11:44:30 -0500

    logica calculos TCO

--- src/pages/tco-sublimacion/tco-sublimacion.ts
+++ src/pages/tco-sublimacion/tco-sublimacion.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-
 /**
  * Generated class for the TcoSublimacionPage page.
  *
@@ -15,7 +14,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'tco-sublimacion.html',
 })
 export class TcoSublimacionPage {
-
+  costoTinta;
+  resolucion;
+  cobertura;
+  tipoCliente;
+  resultado;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -23,6 +26,44 @@ export class TcoSublimacionPage {
     console.log('ionViewDidLoad TcoSublimacionPage');
   }
 
+
+  calcularTcoSublimacion(){
+    let capacidadCartucho = 1000;
+    let costoTintaOriginalml=this.costoTinta/capacidadCartucho; 
+    let coberturaCalculo;
+    let costoTintOriginalM2;
+
+    if(this.resolucion==720){
+      if(this.cobertura==0.17) coberturaCalculo = 1.9;
+      if(this.cobertura==0.45)  coberturaCalculo = 4.9;
+      if(this.cobertura==0.63)  coberturaCalculo = 6.3;
+      if(this.cobertura==0.2)  coberturaCalculo = 2.4;
+    }
+
+    if(this.resolucion==1140){
+      if(this.cobertura==0.17) coberturaCalculo = 2.6;
+      if(this.cobertura==0.45)  coberturaCalculo = 6.3;
+      if(this.cobertura==0.63)  coberturaCalculo = 9.8;
+      if(this.cobertura==0.2)  coberturaCalculo = 3.1;
+    }
+
+    costoTintOriginalM2 = coberturaCalculo*costoTintaOriginalml;
+
+
+
+     console.log("cobertura calculo"); 
+     console.log(coberturaCalculo);
+     console.log("-----"); 
+
+     console.log(this.costoTinta);
+     console.log(this.resolucion);
+     console.log(this.cobertura);
+     console.log(this.tipoCliente);
+     console.log("valor calculo final");
+     console.log(costoTintOriginalM2); 
+     this.resultado = costoTintOriginalM2;
+
+  }
 
 
 
